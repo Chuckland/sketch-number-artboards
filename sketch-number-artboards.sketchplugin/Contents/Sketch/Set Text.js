0121e662ad58272aed0bfafc2f00427e3d88f90c
Anton.Dordzheev <anton.dordzheev@gmail.com>
2016-01-16 01:15:11 +0500

    Cleaned up "Set text" plugin. Fixed issue about old value pasting

--- sketch-number-artboards.sketchplugin/Contents/Sketch/Set Text.js
+++ sketch-number-artboards.sketchplugin/Contents/Sketch/Set Text.js
@@ -1,118 +1,59 @@
-@import "shared.js"
+@import 'shared.js'
+
+var getUserResponse = function() {
+    var userInterface = COSAlertWindow.new();
+    var textField = NSTextField.alloc().initWithFrame(NSMakeRect(0,0,250,25));
+
+    userInterface.setMessageText("Set Text");
+    userInterface.addButtonWithTitle("Apply");
+    userInterface.addButtonWithTitle("Cancel");
+
+    textField.setStringValue("");
+
+    userInterface.addAccessoryView(textField);
+
+    return {
+        result: userInterface.runModal(),
+        text: textField.stringValue()
+    };
+};
+
+var setText = function(text, layers) {
+    var layer, oldValue, textToSet;
+    var applyAtLeastOnce = false;
+    for (var i=0; i<layers.count(); i++) {
+        layer = layers[i];
+        if (layer.class() == MSTextLayer) {
+            applyAtLeastOnce = true;
+            oldValue = layer.stringValue();
+            textToSet = text.replace(/\*/g, oldValue);
+            layer.setStringValue(textToSet);
+            layer.setName(textToSet);
+        }
+    }
+    return applyAtLeastOnce;
+};
 
 var onRun = function(context) {
-    // Проверить, что все выбранные слои текстовые
     var doc = context.document;
     var selection = context.selection;
     var APPLY = 1000;
-    var READY_TO_SET = true;
-    var CANCELLED = false;
-    var NOT_READY = null;
-    var textToSet;
-    var useOldValue;
 
     if (selection.count() == 0) {
         doc.showMessage('Select at least one text layer');
         return;
     }
 
-    userInterfaceLoop();
-
-    function userInterfaceLoop() {
-
-        var uiResponse = NOT_READY;
-
-        while (uiResponse === NOT_READY) {
-
-            // Creatte the interface
-            var modal = createUserInterface();
-
-            // Show it and process the form
-            uiResponse = processButtonClick(modal, modal.runModal());
-
-            // Process the response
-            switch (uiResponse) {
-
-                // Reload the interface
-                case NOT_READY:
-                alert("Find or replace cannot be blank");
-                break;
-
-                // Let's go
-                case READY_TO_SET:
-                doSetText();
-                break;
-
-                // Cancelled
-                case CANCELLED:
-                doc.showMessage("Cancelled");
-                break;
+    var response = getUserResponse();
+    if (response.result === APPLY) {
+        var text = response.text.trim();
+        if (text.length !== 0) {
+            var applyAtLeastOnce = setText(text, selection);
+            if (!applyAtLeastOnce) {
+                doc.showMessage('Select at least one text layer');
             }
-        }
-    }
-
-    function createUserInterface() {
-        var userInterface = COSAlertWindow.new();
-
-        userInterface.setMessageText("Set Text");
-
-        userInterface.addTextFieldWithValue("");
-
-        userInterface.addButtonWithTitle("Apply");
-        userInterface.addButtonWithTitle("Cancel");
-
-        return userInterface;
-    }
-
-    function processButtonClick(modal, buttonClick) {
-        var result;
-
-        if (buttonClick === APPLY) {
-
-            // Найти *, если есть, выставить флаг «Использовать текущий текст»
-            var stringValue = [[modal viewAtIndex: 0] stringValue];
-            useOldValue = stringValue.match(/\*/g) ? true : false;
-
-            // Grab the data from the form
-            textToSet = stringValue;
-
-            // Make sure we have both text to find and replace
-            if (textToSet != "") {
-
-                // Yeah, ready to go
-                result = READY_TO_SET;
-
-            } else {
-
-                // Need something in find and replace
-                result = NOT_READY;
-
-            }
-
         } else {
-
-            // Cancel button pressed
-            result = CANCELLED;
-
-        }
-
-        return result;
-    }
-
-    function doSetText() {
-        var layer;
-        var oldValue;
-
-        for (var i=0; i<selection.count(); i++) {
-            layer = selection[i];
-            if (layer.class() == MSTextLayer) {
-                if (useOldValue) {
-                    oldValue = layer.stringValue();
-                    textToSet = textToSet.replace(/\*/g, oldValue);
-                }
-                layer.setStringValue(textToSet);
-                layer.setName(textToSet);
-            }
+            doc.showMessage('Text cannot be blank');
         }
     }
 }
